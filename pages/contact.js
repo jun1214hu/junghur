9c97e3d590516dd6601fc7d2f855f8f622cb27df
Jung Hur <huh12141@gmail.com>
2023-02-27 22:24:59 -0500

    content page css created

--- pages/contact.js
+++ pages/contact.js
@@ -2,9 +2,23 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="center">
+    <div>
+    <div className="content">
       <h2>Contact Page</h2>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tristique, nunc vitae consectetur sagittis, nisl eros finibus risus, in semper sem tortor non lacus. Praesent hendrerit a velit eu ultrices. Suspendisse potenti. Aenean consectetur quis quam sed egestas. Donec sed feugiat ante, id tincidunt lectus. Proin dictum nisl erat, sed eleifend quam bibendum nec. Etiam ut justo condimentum, ornare nulla sit amet, venenatis lectus. Vivamus venenatis augue sapien, nec sagittis augue ultricies et. Integer quis ligula vitae urna dapibus tincidunt sit amet eget enim.
+
+Nunc eget euismod tellus, sed rutrum urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse massa lectus, porttitor eget ligula non, vulputate ultricies turpis. Ut id consectetur augue. Morbi congue vestibulum dolor et consectetur. Nunc aliquam magna non aliquam mattis. Aenean tincidunt dui ligula, eget varius eros pharetra quis. Quisque et consectetur justo.
+
+Aliquam id mi et libero tincidunt viverra. Nunc vulputate mauris lorem. Nam sit amet sapien pharetra, mollis ipsum mattis, placerat massa. In placerat odio enim, a venenatis augue lobortis vel. Proin et luctus ligula, et fringilla ex. In hac habitasse platea dictumst. Nullam viverra tellus quis nulla aliquet congue. Donec a vehicula orci, et tincidunt dui. Proin rhoncus nibh id nulla consequat facilisis. Aliquam erat volutpat. Nam velit eros, placerat non urna vitae, interdum varius ex. Sed magna odio, feugiat in lobortis nec, pharetra in ante.
+
+Nunc in augue dui. Maecenas vitae suscipit nunc. Ut pulvinar neque nec feugiat pulvinar. Sed aliquet vitae mi a congue. Nulla finibus in metus eget tristique. Aliquam egestas eu tellus quis eleifend. Duis ornare purus in sapien tincidunt, vel eleifend lorem ullamcorper. Aliquam ut volutpat nibh. Curabitur pellentesque cursus enim, ac pellentesque ante tincidunt non. Praesent dictum eleifend nibh, et sodales tellus. Aenean suscipit nunc eget mi aliquet mollis. Mauris tristique laoreet nisl, quis vehicula magna viverra et. Vivamus id placerat est, at pretium nibh. Sed fermentum metus purus, ut convallis nulla laoreet eu. Fusce eget mollis dolor. Etiam sit amet libero vel lorem molestie molestie sit amet at urna.
+
+Phasellus hendrerit sapien eget ornare sollicitudin. Phasellus rhoncus rutrum nulla nec mattis. Mauris dictum rhoncus nisl vitae rhoncus. Proin vestibulum viverra diam varius condimentum. Donec nec sagittis justo. Vestibulum nec magna suscipit, cursus massa eu, gravida nibh. Quisque sollicitudin mauris nec nulla tincidunt, in suscipit velit fringilla. Duis blandit luctus ex, et venenatis justo euismod vitae. Vivamus dui dui, aliquet et tempus at, blandit sit amet nisi. Nunc nec rhoncus ex. Aenean tincidunt leo a magna dignissim gravida. Vivamus auctor ultricies magna, vitae congue leo commodo ut. Proin at dapibus leo, sit amet faucibus massa. Pellentesque tempor vel nunc nec interdum. Quisque malesuada interdum justo, ac porta est malesuada et.
+      </p>
     </div>
+    </div>
+
   );
 };
 export default Contact;
